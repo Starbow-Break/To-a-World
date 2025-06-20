e7ef6512d796eecb978d1ba7700df5b6f88f6625
gunmango <iamdoorpark@gmail.com>
2025-06-20 14:24:30 +0900

    핸드폰 카메라 연결

--- To a World/Assets/Scripts/Phone/App/PhotoCamera/PhotoCameraWindow.cs
+++ To a World/Assets/Scripts/Phone/App/PhotoCamera/PhotoCameraWindow.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Phone
 {
     public class PhotoCameraWindow : MonoBehaviour, IAppWindow
     {
+        [SerializeField] private RawImage screen;
+        public RawImage Screen => screen;
+        
         public void Open()
         {
             gameObject.SetActive(true);
