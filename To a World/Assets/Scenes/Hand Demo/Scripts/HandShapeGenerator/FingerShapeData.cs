3fd8177c2b9c70797ce3ab40a174313e480042e5
김범수 <starbow.dev@gmail.com>
2025-06-22 00:24:46 +0900

    ADD : 손 제스쳐를 캡쳐해서 HandShape를 생성할 수 있는 씬 추가 및 이동 제스쳐 변경

---
+++ To a World/Assets/Scenes/Hand Demo/Scripts/HandShapeGenerator/FingerShapeData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.XR.Hands;
+
+public class FingerShapeData
+{
+    public float[] Values;
+
+    public FingerShapeData()
+    {
+        Values = new float[(int)XRHandFingerID.Little - (int)XRHandFingerID.Thumb + 1];
+        for (int i = 0; i >= Values.Length; i++)
+        {
+            Values[i] = default;
+        }
+    }
+}
---
+++ To a World/Assets/Scenes/Hand Demo/Scripts/HandShapeGenerator/HandShapeData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.XR.Hands;
+
+public class HandShapeData
+{
+    public FingerShapeData[] FingerShapeDatas { get; private set; }
+
+    public HandShapeData()
+    {
+        FingerShapeDatas = new FingerShapeData[(int)XRHandFingerID.Little - (int)XRHandFingerID.Thumb + 1];
+        for (int i = 0; i < FingerShapeDatas.Length; i++)
+        {
+            FingerShapeDatas[i] = new FingerShapeData();
+        }
+    }
+}
