50ccd91918fade0d1ed8dac12d576ad72bd2b450
김범수 <starbow.dev@gmail.com>
2025-07-07 22:07:47 +0900

    FIX : NPC 감지에서 TTS에 필요한 정보 보내기 및 감지된 NPC만 로딩 표시

---
+++ To a World/Assets/Scripts/NPC/NpcInfo.cs
@@ -0,0 +1,31 @@
+﻿using TTSSystem;
+
+public class NpcInfo
+{
+    public string Language { get; private set; }
+    public string Character { get; private set; }
+
+    public NpcInfo(string language, string characters)
+    {
+        Language = TTSConstants.Defaults.Language;
+        Character = TTSConstants.Defaults.Character;
+        
+        foreach (var lan in TTSConstants.Languages.GetValues())
+        {
+            if (lan == language)
+            {
+                Language = lan;
+                break;
+            }
+        }
+        
+        foreach (var chara in TTSConstants.Characters.GetValues())
+        {
+            if (chara == characters)
+            {
+                Character = chara;
+                break;
+            }
+        }
+    }
+}
