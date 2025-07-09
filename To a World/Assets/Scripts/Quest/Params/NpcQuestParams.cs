42a6487ec63accb52b66f7ebe9560707ce9b14c6
김범수 <starbow.dev@gmail.com>
2025-07-09 16:25:50 +0900

    ADD : 추가된 퀘스트에 대한 Params 추가

--- To a World/Assets/Scripts/Quest/Params/NpcQuestParams.cs
+++ To a World/Assets/Scripts/Quest/Params/NpcQuestParams.cs
@@ -1,4 +1,10 @@
-﻿public class NpcQuestParams : AQuestParams
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NpcQuestParams", 
+    menuName = "ScriptableObjects/QuestStepParams/NpcQuestParams", 
+    order = 2)]
+
+public class NpcQuestParams : AQuestParams
 {
     
 }
