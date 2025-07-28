a26158e14de8bfeea9015611362ace18cf7da296
김범수 <starbow.dev@gmail.com>
2025-07-28 16:10:08 +0900

    ADD : 카메라 퀘스트 파라미터 추가 및 Registry 코드 수정

--- To a World/Assets/Scripts/Quest/Events/CameraEvents.cs
+++ To a World/Assets/Scripts/Quest/Events/CameraEvents.cs
@@ -6,6 +6,6 @@ public class CameraEvents: IEvents
     public event Action<Camera> OnCameraShot;
     public void CameraShot(Camera camera) => OnCameraShot?.Invoke(camera);
 
-    public event Action<Camera, CameraDetectable> OnShotInCamera;
-    public void ShotInCamera(Camera camera, CameraDetectable detectable) => OnShotInCamera?.Invoke(camera, detectable);
+    public event Action<string> OnShotInCamera;
+    public void ShotInCamera(string objectId) => OnShotInCamera?.Invoke(objectId);
 }
