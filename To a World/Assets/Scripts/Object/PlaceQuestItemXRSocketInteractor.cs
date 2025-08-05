404acb571d9bfd956b758b5f38a9119c15325ca4
김범수 <starbow.dev@gmail.com>
2025-08-05 14:22:23 +0900

    ADD : Placable Item에 올바른 소켓에 놓여 퀘스트가 완료되면 일정 시간 이후 파괴되도록 구현

--- To a World/Assets/Scripts/Object/PlaceQuestItemXRSocketInteractor.cs
+++ To a World/Assets/Scripts/Object/PlaceQuestItemXRSocketInteractor.cs
@@ -1,12 +1,16 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 [RequireComponent(typeof(XRSocketInteractor))]
 public class ItemXRSocketInteractor: MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] private ItemSocketData _itemSocketData;
     [SerializeField] private GameObject _visualizer;
+    [SerializeField, Min(0.0f)] private float _inactiveDelay = 0.1f;
 
     private string _id;
     private XRSocketInteractor _interactor;
@@ -19,13 +23,17 @@ public class ItemXRSocketInteractor: MonoBehaviour
 
     private void OnEnable()
     {
-        _interactor.selectEntered.AddListener(PlaceTargetItem);
+        _interactor.selectEntered.AddListener(PlaceItem);
         GameEventsManager.GetEvents<QuestEvents>().OnQuestStateChange += QuestStateChange;
     }
 
     private void OnDisable()
     {
-        _interactor.selectEntered.AddListener(PlaceTargetItem);
+        _interactor.selectEntered.AddListener(PlaceItem);
+        if (GameEventsManager.TryGetEvents<QuestEvents>(out var questEvents))
+        {
+            questEvents.OnQuestStateChange -= QuestStateChange;
+        }
     }
 
     private void QuestStateChange(AQuest quest)
@@ -34,12 +42,26 @@ public class ItemXRSocketInteractor: MonoBehaviour
         if (piQuest != null && piQuest.TargetSocketId == _id)
         {
             bool active = piQuest.State == EQuestState.IN_PROGRESS || piQuest.State == EQuestState.CAN_FINISH;
-            _visualizer.SetActive(active);
-            _interactor.enabled = active;
+            if (active)
+            {
+                _visualizer.SetActive(true);
+                _interactor.socketActive = true;
+            }
+            else
+            {
+                _visualizer.SetActive(false);
+                SocketInactiveSequence().Forget();
+            }
         }
     }
 
-    private void PlaceTargetItem(SelectEnterEventArgs args)
+    private async UniTaskVoid SocketInactiveSequence()
+    {
+        await UniTask.WaitForSeconds(_inactiveDelay);
+        _interactor.socketActive = false;
+    }
+
+    private void PlaceItem(SelectEnterEventArgs args)
     {
         var interactable = args.interactableObject;
         var item = interactable.transform.GetComponent<Item>();
